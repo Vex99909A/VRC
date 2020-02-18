dc8bbb4d9b26057b094b884abf077b70c95c4a0d
yunliyun <46965053+yunliyun@users.noreply.github.com>
2020-02-17 23:04:12 -0500

    <test>

--- src/main.cpp
+++ src/main.cpp
@@ -112,7 +112,7 @@ void autonomous() {
   	//print "opcontrol has started" on brain screen
   	pros::lcd::set_text(0, "opcontrol has started!");
 
- 	//start tasks here
+ 	//start tasks here now
  	pros::Task DriveTrain(DriveTrain_fn, (void*)"PROS", TASK_PRIORITY_DEFAULT, TASK_STACK_DEPTH_DEFAULT, "My Task");
 	pros::Task Lift(Lift_fn, (void*)"PROS", TASK_PRIORITY_DEFAULT, TASK_STACK_DEPTH_DEFAULT, "My Task");
 
